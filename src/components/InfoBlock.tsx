611d8031a234a1242f158953c0defe59b2233cc8
Roman <kybik_rybik1994@mail.ru>
2022-07-13 20:09:36 +0300

    create_react_app_test_SKDesign

---
+++ src/components/InfoBlock.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+const StyledInfoBlock = styled.h1`
+color
+`;
+
+const InfoBlock: FC = () => {
+  return <div></div>;
+};
+
+export default InfoBlock;
---
+++ src/components/RegistrationForm.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+const StyledRegistrationForm = styled.h1`
+color
+`;
+
+const RegistrationForm: FC = () => {
+  return <div></div>;
+};
+
+export default RegistrationForm;
